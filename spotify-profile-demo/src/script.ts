b992e6601f41204220f5007ea34c1904f86f3206
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2025-01-04 17:40:29 +0100

    added fetch profile function

--- spotify-profile-demo/src/script.ts
+++ spotify-profile-demo/src/script.ts
@@ -14,7 +14,11 @@ if (!code) {
 }
 
 async function fetchProfile(token: string): Promise<any> {
-    // TODO: Call Web API
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    return await result.json();
 }
 
 function populateUI(profile: any) {
