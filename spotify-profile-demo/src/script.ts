c4a7c71472a5673a03015c39f7dff8efc31943b2
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2025-01-04 20:05:12 +0100

    added refresh possibility
    removed the code from params post requesting the access token and stored the access token in sessionStorage to avoid re-loggins on refresh

--- spotify-profile-demo/src/script.ts
+++ spotify-profile-demo/src/script.ts
@@ -5,10 +5,27 @@ const clientId = myClientId ; // Replace with your client id
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
-if (!code) {
+// Check if an access token is already stored
+const storedToken = sessionStorage.getItem("accessToken");
+
+if (storedToken) {
+    // Use the stored token to fetch the profile
+    const profile = await fetchProfile(storedToken);
+    populateUI(profile);
+} else if (!code) {
     redirectToAuthCodeFlow(clientId);
 } else {
+    //requesting access token by providing the code
     const accessToken = await getAccessToken(clientId, code);
+    //storing access token
+    sessionStorage.setItem("accessToken", accessToken); 
+
+    //delete the code because this one is not useful anymore
+    params.delete("code");
+    //cleaning the URL - removing the code
+    window.history.replaceState({}, document.title, `${window.location.pathname}?${params}`);
+
+    //fetch and display the profile
     const profile = await fetchProfile(accessToken);
     populateUI(profile);
 }
